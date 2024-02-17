4b8d0b5bee55480e5a53960b3be3e25bbddec993
Narendra Chandratre <nsc@mega.co.nz>
2024-02-18 03:11:56 +0530

    Update paths.py

--- Config/paths.py
+++ Config/paths.py
@@ -1,6 +1,11 @@
 import os
+
 PROJECT_DIRECTORY = os.getcwd()
 
-OUTPUT_PATH = os.path.join(PROJECT_DIRECTORY, 'Output/')
-SCREENSHOT_PATH = os.path.join(OUTPUT_PATH, 'Screenshots/')
-REPORTS_PATH = os.path.join(OUTPUT_PATH, 'Reports/')
+OUTPUT_PATH = os.path.join(PROJECT_DIRECTORY, 'output/')
+SCREENSHOT_PATH = os.path.join(OUTPUT_PATH, 'screenshots/')
+REPORTS_PATH = os.path.join(OUTPUT_PATH, 'reports/')
+
+DEPENDANCIES_PATH = os.path.join(PROJECT_DIRECTORY, 'dependencies/')
+MAC_CHROMEDRIVER_PATH = os.path.join(DEPENDANCIES_PATH, 'chromedriver_mac_arm64/')
+CHROMEDRIVER_PATH = os.path.join(PROJECT_DIRECTORY, '')
